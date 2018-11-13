8dbb16c626bfadcca432c74a93381f53a1805899
femke77 <megan.meyers.388@my.csun.edu>
2018-11-12 16:42:19 -0800

    fixing import

--- routes/movies.js
+++ routes/movies.js
@@ -4,6 +4,11 @@ const router = express.Router();
 const {Movie} = require('../models/movie');
 
 
+router.get('/', async (req, res) => {
+    const movie = await Movie.find().sort({year: "ascending"});
+    if (!movie) return res.status(404).send('Movie not found. Please verify id is correct.');
+    res.send(movie);
+});
 
 router.get('/:id', async (req, res) => {
     const movie = await Movie.findOne({id: req.params.id});
